547b1472f1d8b89377570ceffd3c1f0745d03e68
wli916 <changge31@gmail.com>
2025-02-08 18:29:34 +1300

    fix: rebuild whole the structure

---
+++ main.py
@@ -0,0 +1,12 @@
+from utilities import subpages_setter
+
+
+def main():
+    """ streamlit run main.py """
+    # Set the subpages on the sidebar
+    subpages_setter()
+    # Set the sidebar parameters
+
+
+if __name__ == "__main__":
+    main()
