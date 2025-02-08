e7a266cb5aace75beadd4c182d7254bfb3b07a7d
wli916 <changge31@gmail.com>
2025-02-08 19:07:18 +1300

    feat: add the function of download button to download a model

--- subpages/models.py
+++ subpages/models.py
@@ -1,6 +1,7 @@
-from streamlit import title, divider, expander, caption, empty
+from stqdm import stqdm
+from streamlit import title, divider, expander, caption, empty, sidebar, spinner
 
-from utilities import sidebar_params_download
+from utilities import sidebar_params_download, scope_model_downloader
 
 title("Model Download")
 divider()
@@ -11,3 +12,8 @@ empty_message = empty()
 
 # Set the sidebar parameters
 parameters = sidebar_params_download()
+if parameters:
+    if sidebar.button("Download Model", help="Click to download the model"):
+        with stqdm(desc="Downloading the model...", miniters=1.0):
+            with spinner("Downloading the model..."):
+                scope_model_downloader(parameters["model_name"])
