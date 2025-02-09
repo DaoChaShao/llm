f5f94e3454e136aacf1f1edee8b99d3d9b6b8ad9
syu770 <cutedou2799@vip.qq.com>
2025-02-09 14:45:09 +1300

    feat: complete a new function to generate a text area

--- utilities.py
+++ utilities.py
@@ -1,12 +1,13 @@
 from modelscope import snapshot_download
-from streamlit import Page, navigation, sidebar, header, selectbox
+from streamlit import (Page, navigation, sidebar, header, selectbox,
+                       text_area)
 
 
 def subpages_setter() -> None:
     """ Set the subpages on the sidebar """
-    subpage_pages: list = ["subpages/home.py", "subpages/models.py"]
-    subpage_titles: list = ["Home", "Model Download"]
-    subpage_icons: list = [":material/home:", ":material/download:"]
+    subpage_pages: list = ["subpages/home.py", "subpages/models.py", "subpages/text2images.py"]
+    subpage_titles: list = ["Home", "Model Download", "Text Transfer"]
+    subpage_icons: list = [":material/home:", ":material/download:", ":material/photo_library:"]
 
     subpages: dict = {
         "Introduction": [
@@ -14,18 +15,19 @@ def subpages_setter() -> None:
         ],
         "Actions": [
             Page(page=subpage_pages[1], title=subpage_titles[1], icon=subpage_icons[1]),
+            Page(page=subpage_pages[2], title=subpage_titles[2], icon=subpage_icons[2]),
         ],
     }
     pages = navigation(subpages)
     pages.run()
 
 
-def sidebar_params_download():
+def sidebar_params_download() -> dict:
     parameters = {}
 
     with sidebar:
         header("Parameters")
-        models: list = ["qwen/Qwen2.5-0.5B-Instruct", ]
+        models: list = ["qwen/Qwen2.5-7B-Instruct-GGUF", ]
         model_name = selectbox("Model Name", ["Select"] + models)
         if model_name != "Select":
             parameters["model_name"] = model_name
@@ -41,3 +43,11 @@ def scope_model_downloader(model_name: str):
         revision="master",
         max_workers=8,
     )
+
+
+def zone_text() -> str:
+    input_text = text_area(
+        "Content", placeholder="Type something here", height=600,
+        help="This is a text area. You can type a lot of text here."
+    )
+    return input_text
