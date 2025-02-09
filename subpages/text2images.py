e2bc1ba7b751a5406255b6218b7d552b72ec5a86
syu770 <cutedou2799@vip.qq.com>
2025-02-09 15:56:49 +1300

    perf: promote the performance of the system in text2image and image2text tasks.

--- subpages/text2images.py
+++ subpages/text2images.py
@@ -1,11 +1,15 @@
-from streamlit import text_input, sidebar, markdown
+from streamlit import text_input, sidebar, markdown, empty
 
-from utilities import zone_text
+from utilities import zone_text, text2images_setter
 
+empty_message: empty = empty()
+
+TITLE_LENGTH: int = 30
 title: str = text_input(
-    "Title", max_chars=30, placeholder="Enter a title here", type="default",
+    "Title", max_chars=TITLE_LENGTH, placeholder="Enter a title here", type="default",
     help="Create a title for your content here."
 )
+
 if title:
     title_level: int = sidebar.slider(
         "Font Size", min_value=1, max_value=6, value=3, step=1, format="%d",
@@ -18,9 +22,16 @@ if title:
     )
     content_length: int = sidebar.slider(
         "Content Size", min_value=100, max_value=900, value=300, step=100, format="%d",
-        help="Adjust the size of the content inside the text zone."
+        help="Before your enter, adjust the size of the content zone FIRSTLY."
     )
     content: str = zone_text(zone_height, content_length)
+
     if content:
         markdown(f"{'#' * title_level} {title}")
         markdown(content)
+        text2images_setter(title, TITLE_LENGTH, empty_message)
+
+    else:
+        empty_message.error("Please enter content for your text zone.")
+else:
+    empty_message.error("Please enter a title for your content.")
