7820c7176faa4ee4c5640b9efd3e04bfd636e845
syu770 <cutedou2799@vip.qq.com>
2025-02-09 18:18:58 +1300

    feat: modify and promote the function of text2images.py; simultaneously, add a new function that generates text-based images.

--- subpages/text2images.py
+++ subpages/text2images.py
@@ -1,35 +1,41 @@
-from streamlit import text_input, sidebar, markdown, empty
+from streamlit import title, divider, text_input, sidebar, markdown, empty
 
 from utilities import zone_text, text2images_setter
+
+title("Text2Images")
+divider()
 
 empty_message: empty = empty()
 
 TITLE_LENGTH: int = 30
-title: str = text_input(
+title_text: str = text_input(
     "Title", max_chars=TITLE_LENGTH, placeholder="Enter a title here", type="default",
     help="Create a title for your content here."
 )
 
-if title:
-    title_level: int = sidebar.slider(
-        "Font Size", min_value=1, max_value=6, value=3, step=1, format="%d",
-        help="Adjust the font size of your content."
-    )
+if title_text:
     options: list = [300, 400, 500, 600]
     zone_height: int = sidebar.slider(
         "Zone Height", min_value=300, max_value=600, value=300, step=100, format="%d",
         help="Adjust the height of the text zone."
     )
-    content_length: int = sidebar.slider(
-        "Content Size", min_value=100, max_value=900, value=300, step=100, format="%d",
+    content_max: int = sidebar.slider(
+        "Content Max Length", min_value=100, max_value=900, value=300, step=100, format="%d",
         help="Before your enter, adjust the size of the content zone FIRSTLY."
     )
-    content: str = zone_text(zone_height, content_length)
+    content_text: str = zone_text(zone_height, content_max)
+    title_size: int = sidebar.slider(
+        "The Font Size of Your Title", min_value=100, max_value=240, value=168, step=2, format="%d",
+        help="Adjust the fonts size of your content."
+    )
+    content_size: int = sidebar.slider(
+        "The Font Size of Your Content", min_value=12, max_value=36, value=16, step=2, format="%d",
+        help="Before your enter, adjust the size of the content zone FIRSTLY."
+    )
+    sidebar.divider()
 
-    if content:
-        markdown(f"{'#' * title_level} {title}")
-        markdown(content)
-        text2images_setter(title, TITLE_LENGTH, empty_message)
+    if content_text.strip():
+        text2images_setter(title_text, title_size, content_text, content_size, empty_message)
 
     else:
         empty_message.error("Please enter content for your text zone.")
