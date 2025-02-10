a77b6c4d1074e9c3f1abbc97f36df46f285f3092
syu770 <cutedou2799@vip.qq.com>
2025-02-10 17:34:06 +1300

    chore: update the introduction

--- subpages/1_home.py
+++ subpages/1_home.py
@@ -5,6 +5,6 @@ divider()
 with expander("The Application Introduction", expanded=True):
     caption("This application is designed to practice setting, modifying, and ragging the LLM model.")
     caption("1. [x] Download the model from **[ModelScope](https://www.modelscope.cn/)**.")
-    caption("2. [] Load the downloaded model into the application with ModelScope model loader.")
+    caption("2. Load the downloaded model into the application with ModelScope model loader.")
 
 empty_message = empty()
