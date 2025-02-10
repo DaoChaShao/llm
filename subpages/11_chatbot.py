dffe88f77d4858aeb91312513a73ded4e1dfd9b3
syu770 <cutedou2799@vip.qq.com>
2025-02-10 18:23:01 +1300

    perf: add collect the parameters function of accessing the online model

--- subpages/11_chatbot.py
+++ subpages/11_chatbot.py
@@ -1,5 +1,24 @@
-from streamlit import chat_input
+from streamlit import (chat_input, sidebar, header, selectbox, caption,
+                       text_input)
 from streamlit_chat import message
+
+with sidebar:
+    header("Model Selection")
+    options: list[str] = ["Online", "Offline"]
+    option: str = selectbox(
+        "Select a model", ["Select"] + options, placeholder="Select a model",
+        help="Select a model to use for text generation"
+    )
+    if option in options:
+        match option:
+            case "Online":
+                caption("Using the deepseek api key to access the model")
+                api_key: str = text_input("Enter the API key", value="", type="password", max_chars=36)
+                if api_key:
+                    # TODO: call the deepseek api to get the model
+                    pass
+            case "Offline":
+                caption("Using the local model for text generation")
 
 message(
     "Hello, how are you?",
