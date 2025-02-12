6729d3603ec443f8759c963ec1d320baefdc9ea8
syu770 <27882272@qq.com>
2025-02-12 16:25:08 +1300

    feat: add the new function about Milvus database.

--- utilities/llm.py
+++ utilities/llm.py
@@ -1,5 +1,6 @@
 from modelscope import snapshot_download, AutoModelForCausalLM, AutoTokenizer
-from streamlit import sidebar, header, selectbox, empty
+from pymilvus import connections, db
+from streamlit import sidebar, header, selectbox, empty, write
 
 
 def sidebar_params_download(message: empty) -> dict:
@@ -73,3 +74,34 @@ def model_loader_scope(params: dict, sys_content: str, prompt: str) -> str:
 def model_loader_deepseek_api(api_key: str, sys_content: str, prompt: str):
     # TODO: Implement the deepseek API
     pass
+
+
+def sidebar_params_kb(message: empty) -> None:
+    uploaded_files = sidebar.file_uploader(
+        "Choose a CSV file", accept_multiple_files=True
+    )
+    if uploaded_files:
+        for uploaded_file in uploaded_files:
+            bytes_data = uploaded_file.read()
+            write("filename:", uploaded_file.name)
+            write(bytes_data)
+    else:
+        message.error("Please upload a CSV file.")
+
+
+class MilvusDB(object):
+    def __init__(self, schema: str = "default"):
+        # self._message = message
+        self._schema = schema
+
+    def __enter__(self):
+        connections.connect(alias=self._schema, host="localhost", port="19530")
+        if db.connections.has_connection(self._schema):
+            # self._message.success("Connection established successfully.")
+            print("Connection established successfully.")
+        else:
+            # self._message.error("Connection failed.")
+            print("Connection failed.")
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        connections.disconnect(alias=self._schema)
