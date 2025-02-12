b24195d91d819ccd66bae7d18a20163114663139
syu770 <27882272@qq.com>
2025-02-12 15:11:03 +1300

    perf: rebuild the structure of all functions

---
+++ utilities/layout.py
@@ -0,0 +1,44 @@
+from streamlit import Page, navigation
+
+
+def subpages_setter() -> None:
+    """ Set the subpages on the sidebar """
+    pages: dict = {
+        "page": [
+            "subpages/00_home.py",
+            "subpages/10_models.py",
+            "subpages/11_chatbot.py",
+            "subpages/20_title2images.py",
+            "subpages/21_content2images.py",
+        ],
+        "title": [
+            "Home",
+            "Model Download",
+            "ChatBot",
+            "Title to Image Transfer",
+            "Content to Image Transfer",
+        ],
+        "icon": [
+            ":material/home:",
+            ":material/download:",
+            ":material/robot:",
+            ":material/photo_library:",
+            ":material/image:"
+        ],
+    }
+
+    page_structure: dict = {
+        "Introduction": [
+            Page(page=pages["page"][0], title=pages["title"][0], icon=pages["icon"][0]),
+        ],
+        "LLM Actions": [
+            Page(page=pages["page"][1], title=pages["title"][1], icon=pages["icon"][1]),
+            Page(page=pages["page"][2], title=pages["title"][2], icon=pages["icon"][2]),
+        ],
+        "Red Note": [
+            Page(page=pages["page"][3], title=pages["title"][3], icon=pages["icon"][3]),
+            Page(page=pages["page"][4], title=pages["title"][4], icon=pages["icon"][4]),
+        ],
+    }
+    pg = navigation(page_structure, position="sidebar", expanded=True)
+    pg.run()
