5570075df04a8d979ab4af46a7b463995b7fd86d
DaoChaShao <27882272@qq.com>
2025-02-22 20:03:34 +1300

    fix: rename layout file and update page structure

--- utilis/layout.py
+++ utilis/layout.py
@@ -6,25 +6,16 @@ def subpages_setter() -> None:
     pages: dict = {
         "page": [
             "subpages/00_home.py",
-            "subpages/10_models.py",
-            "subpages/11_chatbot.py",
-            "subpages/12_knowledge_base.py",
             "subpages/20_title2images.py",
             "subpages/21_content2images.py",
         ],
         "title": [
             "Home",
-            "Model Download",
-            "ChatBot",
-            "Knowledge Base",
             "Title to Image Transfer",
             "Content to Image Transfer",
         ],
         "icon": [
             ":material/home:",
-            ":material/download:",
-            ":material/robot:",
-            ":material/book:",
             ":material/photo_library:",
             ":material/image:"
         ],
@@ -34,14 +25,9 @@ def subpages_setter() -> None:
         "Introduction": [
             Page(page=pages["page"][0], title=pages["title"][0], icon=pages["icon"][0]),
         ],
-        "LLM Actions": [
+        "Red Note": [
             Page(page=pages["page"][1], title=pages["title"][1], icon=pages["icon"][1]),
             Page(page=pages["page"][2], title=pages["title"][2], icon=pages["icon"][2]),
-            Page(page=pages["page"][3], title=pages["title"][3], icon=pages["icon"][3]),
-        ],
-        "Red Note": [
-            Page(page=pages["page"][4], title=pages["title"][4], icon=pages["icon"][4]),
-            Page(page=pages["page"][5], title=pages["title"][5], icon=pages["icon"][5]),
         ],
     }
     pg = navigation(page_structure, position="sidebar", expanded=True)
