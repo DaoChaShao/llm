1317d64da013d567cc0e463efb8146a105b0d8c7
DaoChaShao <27882272@qq.com>
2025-02-22 20:02:54 +1300

    feat: rename application and update introduction text

--- subpages/00_home.py
+++ subpages/00_home.py
@@ -1,10 +1,8 @@
 from streamlit import title, divider, expander, caption, empty
 
-title("LLM Application")
+title("Red Note Cover Generator")
 divider()
 with expander("The Application Introduction", expanded=True):
-    caption("This application is designed to practice setting, modifying, and ragging the LLM model.")
-    caption("1. [x] Download the model from **[ModelScope](https://www.modelscope.cn/)**.")
-    caption("2. Load the downloaded model into the application with ModelScope model loader.")
+    caption("This application is designed to practice converting the text to the images.")
 
 empty_message = empty()
